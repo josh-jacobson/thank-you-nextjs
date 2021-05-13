12ccc9edd32704ef0dfcca3ced8688b41f929d69
Josh Jacobson <joshjacobson14@gmail.com>
2021-05-12 17:22:56 -0700

    Ariana Grande is my fave omg

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Thank You, <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          I'm so grateful for my ex. {' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
